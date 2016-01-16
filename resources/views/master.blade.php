0c60fed0fda9095adcf7ac5479d3af67370fb2ea
mrkirby153 <mr.austinwhyte@gmail.com>
2016-01-16 10:54:29 -0800

    Added mobile viewport meta tag

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @yield('title')
     <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
