ce4c667ad52ea74f9ca85f94975b92580176a579
mrkirby153 <mr.austinwhyte@gmail.com>
2016-01-16 10:19:57 -0800

    Basic master layout

---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @yield('title')
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+    @yield('js')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}"/>
+    @yield('css')
+</head>
+<body>
+<div class="container">
+    <div clas="row">
+        <div class="col-xs-12">
+            @yield('content')
+        </div>
+    </div>
+</div>
+</body>
+</html>
