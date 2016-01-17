393cec054eab088f304416549c18a0b2014caca9
mrkirby153 <mr.austinwhyte@gmail.com>
2016-01-17 11:41:02 -0800

    Changed how layout is formatted

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,9 +2,9 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @yield('title')
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <title>FTC Scouting - @yield('title')</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
     @yield('js')
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     @yield('css')
@@ -13,6 +13,10 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
+            <h1>Team 7854 Scouting
+                <small>@yield('subtitle')</small>
+            </h1>
+            <hr/>
             @yield('content')
         </div>
     </div>
