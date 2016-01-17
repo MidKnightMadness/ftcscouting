f3e236b14abf9ebd91111e16b205c2a4855ea43a
mrkirby153 <mr.austinwhyte@gmail.com>
2016-01-17 12:51:13 -0800

    Use the page-header div instead of our own

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,17 +6,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
     @yield('js')
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"
+          crossorigin="anonymous">
     @yield('css')
 </head>
 <body>
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            <h1>Team 7854 Scouting
-                <small>@yield('subtitle')</small>
-            </h1>
-            <hr/>
+            <div class="page-header">
+                <h1>Team 7854 Scouting
+                    <small>@yield('subtitle')</small>
+                </h1>
+            </div>
             @yield('content')
         </div>
     </div>
