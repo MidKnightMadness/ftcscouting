44929cf3ca121490d25be124768b909f711250ea
mrkirby153 <mr.austinwhyte@gmail.com>
2016-01-17 14:17:15 -0800

    Added a universal alert message

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,6 +9,16 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"
           crossorigin="anonymous">
     @yield('css')
+
+    @if(Session::has('alert_msg'))
+        <script type="text/javascript">
+            window.setTimeout(function () {
+                $("#info-alert").fadeTo(2000, 500).slideUp(500, function(){
+                    $("#info-alert").alert('close');
+                })
+            }, 5000);
+        </script>
+        @endif
 </head>
 <body>
 <div class="container">
@@ -19,6 +29,12 @@
                     <small>@yield('subtitle')</small>
                 </h1>
             </div>
+            @if(Session::has('alert_msg'))
+                <div class="alert alert-{{Session::has('alert_msg_type')? Session::get('alert_msg_type') : 'info'}} alert-dismissible" id="info-alert" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <div class="text-center">{{Session::get('alert_msg')}}</div>
+                </div>
+            @endif
             @yield('content')
         </div>
     </div>
