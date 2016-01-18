4021ca99a4bc76b9f97fd9bf33b0a5e16e35f5e9
mrkirby153 <mr.austinwhyte@gmail.com>
2016-01-17 19:08:54 -0800

    Add match results to the portal

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -9,5 +9,6 @@
 @stop
 @section('content')
     {!! Html::link(route('team.new'), 'New Team', array('class'=>'btn btn-success btn-block')) !!}
+    {!! Html::link(route('match.add'), 'New Match Result', array('class'=>'btn btn-danger btn-block')) !!}
     {!! Html::link(route('team.list'), 'View Teams', array('class'=>'btn btn-primary btn-block')) !!}
 @stop
