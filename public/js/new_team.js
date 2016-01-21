87570fed56c2238846d0e5208b87225f494247c1
mrkirby153 <mr.austinwhyte@gmail.com>
2016-01-20 20:59:01 -0800

    Fixed bug where puting in a String for team name returns team number 0
    A bug was discovered while testing the form in that when a person would
    enter a string in for the 'Team Number', it would do an ajax lookup for
    the team number of the provided string. However, the team lookup is done
    via Laravel's Eloquent model system and therefore when looking up the
    team's number (team_number int(11)) in the database, it would try and
    convert the string into a number. The closest numeric representation of
    the string 'Test' is the number 0, so it would return the team with a team
    number of zero.

--- public/js/new_team.js
+++ public/js/new_team.js
@@ -1,14 +1,22 @@
+function disableForm(reason){
+    $("#tn_exist").html(reason);
+    $("#submit_button").prop("disabled", true);
+    $("#tn_div").prop("class", "form-group has-error");
+}
+var validateRegex = new RegExp("[0-9]+");
 $(document).ready(function(){
     $("#team_number").focusout(function(){
         var teamNumber = $("#team_number").val();
         if(teamNumber == null || teamNumber == "")
             return;
+        if(!validateRegex.test(teamNumber)){
+            disableForm(teamNumber+" is not a number in this world. It may be in your world, but not in this one");
+            return;
+        }
         var url = '/ajax/team-info/'+ teamNumber;
         $.getJSON(url, function ( data ){
             if(typeof data.team_name != "undefined") {
-                $("#tn_exist").html("Team "+teamNumber+" has already been submitted by "+data.submitter);
-                $("#submit_button").prop("disabled", true);
-                $("#tn_div").prop("class", "form-group has-error");
+
             } else {
                 $("#submit_button").prop("disabled", false);
                 $("#tn_div").prop("class", "form-group");
