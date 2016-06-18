73c0f19b37c6959eb6ab76c363aa59537fcd0ebb
mrkirby153 <mr.austinwhyte@gmail.com>
2016-06-18 15:30:53 -0700

    Created Eloquent models for database tables

---
+++ app/TeamInvite.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TeamInvite extends Model
+{
+    protected $table = 'team_invites';
+    
+    protected $guarded = [];
+}
