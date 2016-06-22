e7891a458304495cfd56b766fef6422411dbc981
mrkirby153 <mr.austinwhyte@gmail.com>
2016-06-22 12:39:33 -0700

    Heightened security of session and xsrf cookies.
    Session cookies are actual session cookies and expire when the browser is
    closed or after two hours, whichever comes first.
    
    Xsrf cookies are now HttpOnly, preventing the client from accessing them
    via Javascript or other methods.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,9 +3,9 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Symfony\Component\HttpFoundation\Cookie;
 
-class VerifyCsrfToken extends BaseVerifier
-{
+class VerifyCsrfToken extends BaseVerifier {
     /**
      * The URIs that should be excluded from CSRF verification.
      *
@@ -14,4 +14,13 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    // Heighten security of XSRF-TOKEN cookie by making it only accessible over HTTP, preventing the client from accessing it.
+    protected function addCookieToResponse($request, $response) {
+        $config = config('session');
+        $cookie = new Cookie('XSRF-TOKEN', $request->session()->token(), time() + 60 * 120,
+            $config['path'], $config['domain'], $config['secure'], true);
+        $response->headers->setCookie($cookie);
+        return $response;
+    }
 }
