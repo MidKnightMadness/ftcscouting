e378d14c6f179be28d76446b913f7e7ff1baf158
mrkirby153 <mr.austinwhyte@gmail.com>
2016-06-28 20:04:48 -0700

    When the document loads, hide all elements with data-hidden=true

--- resources/assets/js/toggle.js
+++ resources/assets/js/toggle.js
@@ -1,21 +1,30 @@
-$(".toggle").click(function(){
+$(".toggle").click(function () {
     var element = $(this).data('toggle');
     toggleElement(element);
 });
-function toggleElement(clazz){
+function toggleElement(clazz) {
     var toToggle = document.getElementsByClassName(clazz);
-    for(var i = 0; i < toToggle.length; i++){
+    for (var i = 0; i < toToggle.length; i++) {
         doToggle(toToggle[i]);
     }
 }
+$(document).ready(function () {
+    var allToHide = document.querySelectorAll("[data-hidden=true]");
+    for (var i = 0; i < allToHide.length; i++) {
+        var element = $(allToHide[i]);
+        var hidden = element.data('hidden');
+        element.data('hidden', true);
+        element.hide();
+    }
+});
 
-function doToggle(element){
+function doToggle(element) {
     element = $(element);
-    if(!element.attr('data-hidden')){
+    if (!element.attr('data-hidden')) {
         element.attr('data-hidden', false);
     }
     var hidden = element.data('hidden');
-    if(hidden){
+    if (hidden) {
         element.data('hidden', false);
         element.fadeIn(500);
     } else {
