61da4e246d395d872c593e1f07ba220cd9f97fdc
mrkirby153 <mr.austinwhyte@gmail.com>
2016-10-13 19:13:29 -0700

    Removed extra space from team display

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -32,8 +32,7 @@
                 @if(count($part_of) == 0)
                     <p>{{$user->name}} is not a member of any teams</p>
                 @elseif(count($part_of) == 1)
-                    <p>{{$user->name}} is a member of <code>Team {{$part_of[0]->team_number}}
-                            , {{$part_of[0]->name}}</code>
+                    <p>{{$user->name}} is a member of <code>Team {{$part_of[0]->team_number}}, {{$part_of[0]->name}}</code>
                     </p>
                 @else
                     <p>{{$user->name}} is a member of the following teams: </p>
