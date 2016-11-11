33ac0def1cc26096828c4a45ab4dcf18f3383995
mrkirby153 <mr.austinwhyte@gmail.com>
2016-11-10 20:43:16 -0800

    Update 503 error page
    Page is less sarcastic and displays Laravel's down for mantinace options (reason, retry in) when applicable.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -6,6 +6,20 @@
 
 @section('body')
     <h3 class="text-center">Service Unavailable</h3>
-    <p class="text-center">Well... Yeah... This is embarrassing. For whatever reason, we're unavailable. Check back
-        soon!</p>
+    <?php
+    $json = json_decode(file_get_contents(storage_path('framework/down')), true);
+    $message = $json['message'];
+    $retry_after = $json['retry'];
+    ?>
+    <p class="text-center">
+        @if($message)
+            <code>{{$message}}</code>
+        @else
+            Down for maintenance
+        @endif
+        @if($retry_after)
+            <br>
+            Come back {{\Carbon\Carbon::now()->addSeconds($retry_after)->diffForHumans()}}
+        @endif
+    </p>
 @endsection
