fcc4188b6f522e67be0355e0a7794369bece9489
mrkirby153 <mr.austinwhyte@gmail.com>
2017-02-14 20:28:32 -0800

    Add LOG_QUERIES environment variable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,16 +5,25 @@ namespace App\Providers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+    public function boot() {
+        if (env('LOG_QUERIES', false))
+            DB::listen(function ($query) {
+                $sql = $query->sql;
+                $backtrace = debug_backtrace();
+                $log = "Query \"{$sql}\" executed by: \n";
+                foreach ($backtrace as $bt) {
+                    $file = isset($bt['file']) ? $bt['file'] : "Unknown";
+                    $line = isset($bt['file']) ? $bt['line'] : "??";
+                    $log .= "\t{$file}:{$line}\n";
+                }
+                \Log::info($log);
+            });
     }
 
     /**
@@ -22,8 +31,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         $this->app->alias('bugsnag.multi', \Illuminate\Contracts\Logging\Log::class);
         $this->app->alias('bugsnag.multi', \Psr\Log\LoggerInterface::class);
     }
